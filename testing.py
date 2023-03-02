bcc4a536878820f1762bfebe32f44950ed7932f9
Jonathan Tonglet <49620076+jtonglet@users.noreply.github.com>
2023-03-02 14:11:15 +0100

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,9 +1,7 @@
+#Evaluate results
 import r0827509
 
-
 reporter=r0827509.r0827509() 
-
-
 
 reporter.optimize('data/tour50.csv')  #Optimal : 54119, heuristic : 66540  , My score: 54440 
 reporter.optimize('data/tour100.csv') #Optimal : 85807, heuristic :  103436, My score: 85808 
@@ -11,4 +9,3 @@ reporter.optimize('data/tour100.csv') #Optimal : 85807, heuristic :  103436, My 
 # reporter.optimize('data/tour500.csv') #Optimal : 60911, heuristic : 78579, My score :  70295
 # reporter.optimize('data/tour750.csv')  #Optimal : 101258, heuristic : 134752  , My score :  130320 
 # reporter.optimize('data/tour1000.csv') #Optimal :50604, heuristic : 75446, My score : 71415 
-
